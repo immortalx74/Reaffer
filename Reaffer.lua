330823962d4b22aec0ac1a2006f19dead3ecb4c8
immortalx74 <jfdodis@yahoo.com>
2022-05-07 05:29:12 +0300

    Implemented note entry with drag support(pitch+duration)

--- Reaffer.lua
+++ Reaffer.lua
@@ -1,4 +1,7 @@
 -- Reaffer ---------------------------------------------------
+function msg(txt)
+	reaper.ShowConsoleMsg(txt)
+end
 
 local e_EditorState =
 {
@@ -6,12 +9,11 @@ local e_EditorState =
 	MoveReady = 2,
 	DrawReady = 3,
 	EraseReady = 4,
-	BeginNote = 5,
-	CommitNote = 6,
-	PitchCurrent = 7,
-	PitchExisting = 8,
-	ResizeCurrent = 9,
-	ResizeExisting = 10
+	EnterNote = 5,
+	PitchCurrent = 6,
+	PitchExisting = 7,
+	ResizeCurrent = 8,
+	ResizeExisting = 9
 }
 
 local e_Tool = 
@@ -35,6 +37,16 @@ local App =
 	is_open,
 	icon_font,
 	editor_state,
+	mouse_x,
+	mouse_y,
+	mouse_prev_x,
+	mouse_prev_y,
+	drag_x = 0,
+	drag_y = 0,
+	scroll_x = 0,
+	arrange_win_x = 0,
+	arrange_win_y = 0,
+	last_note_pitch = 0,
 	
 	-- metrics
 	window_w = 800,
@@ -63,7 +75,7 @@ local App =
 	
 	-- data
 	quantize = {"1/1", "1/2", "1/4", "1/8", "1/16", "1/32", "1/64"},
-
+	
 	signature = {
 		{caption = "2/4", beats = 2, subs = 4},
 		{caption = "3/4", beats = 3, subs = 4},
@@ -77,23 +89,23 @@ local App =
 		{caption = "4/4 Tri", beats = 4, subs = 3},
 		{caption = "8/4 Tri", beats = 8, subs = 3}
 	},
-
+	
 	instrument =
 	{
-		{num_strings = 4, open = 25, "G2 ", "D2 ", "A1 ", "E1 "},
-		{num_strings = 5, open = 26, "G2 ", "D2 ", "A1 ", "E1 ", "B0 "},
-		{num_strings = 6, open = 25, "E4 ", "B3 ", "G3 ", "D3 ", "A2 ", "E2 "},
-		{num_strings = 7, open = 25, "E4 ", "B3 ", "G3 ", "D3 ", "A2 ", "E2 ", "B1 "},
-		{num_strings = 8, open = 25, "E4 ", "B3 ", "G3 ", "D3 ", "A2 ", "E2 ", "B1 ", "F#1"},
-		{num_strings = 9, open = 25, "E4 ", "B3 ", "G3 ", "D3 ", "A2 ", "E2 ", "B1 ", "F#1", "C#1"}
+		{num_strings = 4, "E1", "A1", "D2", "G2"},
+		{num_strings = 5, "B0", "E1", "A1", "D2", "G2"},
+		{num_strings = 6, "E2", "A2", "D3", "G3", "B3", "E4"},
+		{num_strings = 7, "B1", "E2", "A2", "D3", "G3", "B3", "E4"},
+		{num_strings = 8, "F#1", "B1", "E2", "A2", "D3", "G3", "B3", "E4"},
+		{num_strings = 9, "C#1", "F#1", "B1", "E2", "A2", "D3", "G3", "B3", "E4"}
 	},
-
+	
 	note_sequence = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"},
 	
 	note_list =
 	{
-		{offset = 6, string_idx = 2, num = 25, velocity = 127, duration = 1, selected = false},
-		{offset = 10, string_idx = 4, num = 40, velocity = 24, duration = 3, selected = false}
+		-- {offset = 6, string_idx = 2, pitch = 25, velocity = 127, duration = 1, selected = false},
+		-- {offset = 10, string_idx = 4, pitch = 40, velocity = 24, duration = 3, selected = false}
 	}
 }
 
@@ -106,17 +118,6 @@ local Colors =
 	red = 0xFF0000FF,
 	text
 }
-
-local Editor = {}
-
-function Editor.OnClick(cx, cy)
-	if App.active_tool == e_Tool.Draw then
-		App.editor_state = e_EditorState.BeginNote
-
-		local temp_note = {offset = cx, string_idx = cy, num = 25, velocity = 127, duration = 1, selected = false}
-		App.note_list[#App.note_list + 1] = temp_note
-	end
-end
 
 local ToolBar = 
 {
@@ -169,26 +170,108 @@ function Util.VelocityColor(v)
 	return Util.PackRGBA(r, g, b, 255)
 end
 
-function Util.NoteNumToName(note_num)
-	local mul = math.floor(note_num / 12)
-	local idx = note_num - (mul * 12)
+function Util.NotePitchToName(note_pitch)
+	local mul = math.floor(note_pitch / 12)
+	local idx = note_pitch - (mul * 12)
 	return App.note_sequence[idx + 1] .. mul - 1
 end
 
-function Util.StateHandler()
+function Util.NoteNameToPitch(note_name)
+	
+	local len = string.len(note_name)
+	local wname
+	
+	if len == 2 then
+		wname = string.sub(note_name, 1, 1)
+	else
+		wname = string.sub(note_name, 1, 2)
+	end
+	
+	local idx = 0
+	
+	for i, v in ipairs(App.note_sequence) do
+		if App.note_sequence[i] == wname then
+			idx = i
+			break
+		end
+	end
+	
+	local oct = string.sub(note_name, len, len)
+	local base = 12 -- pitch of C0
+	return (base + idx - 1) + (oct * 12)
+end
+
+local Editor = {}
+
+function Editor.OnClick(cx, cy)
+	if App.active_tool == e_Tool.Draw then
+		App.editor_state = e_EditorState.EnterNote
+		-- Deselect previously selected notes
+		for i, v in ipairs(App.note_list) do
+			App.note_list[i].selected = false
+		end
+		
+		local open_pitch = Util.NoteNameToPitch(App.instrument[App.num_strings - 3][App.num_strings - cy])
+		local new_note = {offset = cx, string_idx = cy, pitch = open_pitch, velocity = 127, duration = 1, selected = true}
+		local idx = #App.note_list + 1
+		App.note_list[idx] = new_note
+		App.last_note_pitch = App.note_list[idx].pitch
+	end
+end
+
+function Editor.OnRelease()
+	if App.active_tool == e_Tool.Draw then
+		App.editor_state = e_EditorState.DrawReady
+		App.drag_x = 0
+		App.drag_y = 0
+	end
+end
+
+function Editor.OnDrag()
+	if App.active_tool == e_Tool.Draw and App.editor_state == e_EditorState.EnterNote then
+		-- Get selected note
+		local sel_idx = 1
+		for i, v in ipairs(App.note_list) do
+			if App.note_list[i].selected then
+				sel_idx = i
+				break
+			end
+		end
+		
+		local cell_x = math.floor((App.mouse_x - App.arrange_win_x + App.scroll_x -15) / 34) - 1
+		local cell_y = math.floor((App.mouse_y - App.arrange_win_y - App.top_margin + 5) / 12)
+		
+		if cell_x >= App.note_list[sel_idx].offset then
+			App.note_list[sel_idx].duration = cell_x - App.note_list[sel_idx].offset + 1
+		end
+		
+		local pitch_min = Util.NoteNameToPitch(App.instrument[App.num_strings - 3][App.num_strings - App.note_list[sel_idx].string_idx])
+		local pitch_max = pitch_min + 24
+		
+		local diff = Util.Clamp(App.note_list[sel_idx].string_idx - cell_y, -(App.last_note_pitch - pitch_min), pitch_max - App.last_note_pitch)
+		App.note_list[sel_idx].pitch = App.last_note_pitch + diff
+		msg(App.note_list[sel_idx].string_idx - cell_y .. "\n")
+	end
 end
 
 local UI = {}
 
-function UI.DrawNotes(draw_list, win_x, win_y, scroll_x)
+function UI.DrawNotes(draw_list)
 	local note_x
 	local note_y
+	local selected
 	
 	for i, v in ipairs(App.note_list) do
-		note_x = win_x + 50 + (App.note_list[i].offset * App.note_w) - scroll_x
-		note_y = win_y + 30 + (App.note_list[i].string_idx * App.note_h) - 5
-		reaper.ImGui_DrawList_AddRectFilled(draw_list, note_x, note_y, note_x + (App.note_w * App.note_list[i].duration) -1, note_y + App.note_h-1, Util.VelocityColor(App.note_list[i].velocity), 40)
-		reaper.ImGui_DrawList_AddText(draw_list, note_x + 5, note_y - 2, Colors.text, Util.NoteNumToName(App.note_list[i].num))
+		if App.note_list[i].string_idx < App.num_strings then -- When switching from higher string count instrument to a lower one, notes are kept but hidden
+			note_x = App.arrange_win_x + 50 + (App.note_list[i].offset * App.note_w) - App.scroll_x
+			note_y = App.arrange_win_y + 30 + (App.note_list[i].string_idx * App.note_h) - 5
+			selected = App.note_list[i].selected
+			reaper.ImGui_DrawList_AddRectFilled(draw_list, note_x, note_y, note_x + (App.note_w * App.note_list[i].duration) -1, note_y + App.note_h-1, Util.VelocityColor(App.note_list[i].velocity), 40)
+			reaper.ImGui_DrawList_AddText(draw_list, note_x + 5, note_y - 2, Colors.text, Util.NotePitchToName(App.note_list[i].pitch))
+			if selected then
+				reaper.ImGui_DrawList_AddRect(draw_list, note_x, note_y, note_x + (App.note_w * App.note_list[i].duration) -1, note_y + App.note_h-1, Colors.text, 40, reaper.ImGui_DrawFlags_None(), 2)
+			end
+		end
 	end
 end
 
@@ -272,7 +355,7 @@ function UI.DrawToolbar()
 				reaper.ImGui_PushStyleColor(App.ctx, reaper.ImGui_Col_Text(), Colors.text)
 			end
 			if reaper.ImGui_Button(App.ctx, ToolBar[i].icon .. "##toolbar_button" .. i) then
-				if i >= 2 and i <= 5 then
+				if i >= 2 and i <= 5 then -- only the note tools can be active
 					App.active_tool = i
 				end
 			end
@@ -302,21 +385,21 @@ function UI.DrawArrange()
 	if reaper.ImGui_BeginChild(App.ctx, "Arrange##win_arrange", App.window_w - App.window_indent, App.arrange_h, true, reaper.ImGui_WindowFlags_HorizontalScrollbar() | reaper.ImGui_WindowFlags_NoMove()) then
 		
 		local draw_list = reaper.ImGui_GetWindowDrawList(App.ctx)
-		local scroll_x = reaper.ImGui_GetScrollX(App.ctx)
-		local win_x, win_y = reaper.ImGui_GetWindowPos(App.ctx)
+		App.scroll_x = reaper.ImGui_GetScrollX(App.ctx)
+		App.arrange_win_x, App.arrange_win_y = reaper.ImGui_GetWindowPos(App.ctx)
 		
 		-- Scroll horizontally with mousewheel
 		local mw = reaper.ImGui_GetMouseWheel(App.ctx)
-		scroll_x = scroll_x - mw * App.wheel_delta
-		reaper.ImGui_SetScrollX(App.ctx, scroll_x)
-		scroll_x = reaper.ImGui_GetScrollX(App.ctx) -- get back clamped value from ImGui
+		App.scroll_x = App.scroll_x - mw * App.wheel_delta
+		reaper.ImGui_SetScrollX(App.ctx, App.scroll_x)
+		App.scroll_x = reaper.ImGui_GetScrollX(App.ctx) -- get back clamped value from ImGui
 		
-		local lane_start_x = win_x + App.left_margin - scroll_x
+		local lane_start_x = App.arrange_win_x + App.left_margin - App.scroll_x
 		local lane_end_x = lane_start_x + lane_w
 		
 		-- Lanes
 		for i = 0, App.num_strings - 1 do
-			reaper.ImGui_DrawList_AddLine(draw_list, lane_start_x, win_y + App.top_margin + (i * App.lane_v_spacing), lane_end_x, win_y + App.top_margin + (i * App.lane_v_spacing), Colors.lane)
+			reaper.ImGui_DrawList_AddLine(draw_list, lane_start_x, App.arrange_win_y + App.top_margin + (i * App.lane_v_spacing), lane_end_x, App.arrange_win_y + App.top_margin + (i * App.lane_v_spacing), Colors.lane)
 		end
 		
 		-- Measures & beats lines and legends
@@ -327,62 +410,72 @@ function UI.DrawArrange()
 			if i % App.signature[App.signature_cur_idx].subs == 0 then
 				
 				if (i ~= 0 and i % (App.signature[App.signature_cur_idx].beats * App.signature[App.signature_cur_idx].subs) == 0) then
-					reaper.ImGui_DrawList_AddLine(draw_list, win_x + App.left_margin + (App.grid_w * i) - scroll_x, win_y + App.top_margin, win_x + App.left_margin + (App.grid_w * i) - scroll_x, win_y + App.top_margin + ((App.num_strings - 1) * 12), Colors.lane)
+					reaper.ImGui_DrawList_AddLine(draw_list, App.arrange_win_x + App.left_margin + (App.grid_w * i) - App.scroll_x, App.arrange_win_y + App.top_margin, App.arrange_win_x + App.left_margin + (App.grid_w * i) - App.scroll_x, App.arrange_win_y + App.top_margin + ((App.num_strings - 1) * 12), Colors.lane)
 					measure_count = measure_count + 1
 					beat_count = 1
 				end
 				
 				if i ~= App.num_grid_divisions then
 					local txt = measure_count .. "-" .. beat_count
-					reaper.ImGui_DrawList_AddTextEx(draw_list, nil, 11, win_x + App.left_margin + (App.grid_w * i) - scroll_x, win_y + App.top_margin - 20, Colors.text, txt)
+					reaper.ImGui_DrawList_AddTextEx(draw_list, nil, 11, App.arrange_win_x + App.left_margin + (App.grid_w * i) - App.scroll_x, App.arrange_win_y + App.top_margin - 20, Colors.text, txt)
 					beat_count = beat_count + 1
 				end
 				
 			else
-				reaper.ImGui_DrawList_AddLine(draw_list, win_x + App.left_margin + (App.grid_w * i) - scroll_x, win_y + App.top_margin - 17, win_x + App.left_margin + (App.grid_w * i) - scroll_x, win_y + App.top_margin - 12, Colors.lane)
+				reaper.ImGui_DrawList_AddLine(draw_list, App.arrange_win_x + App.left_margin + (App.grid_w * i) - App.scroll_x, App.arrange_win_y + App.top_margin - 17, App.arrange_win_x + App.left_margin + (App.grid_w * i) - App.scroll_x, App.arrange_win_y + App.top_margin - 12, Colors.lane)
 			end
 		end
 		
 		-- Notes
-		UI.DrawNotes(draw_list, win_x, win_y, scroll_x)
+		UI.DrawNotes(draw_list)
 		
 		---------------------------------------------------------------------------------------------------------------
 		-- Enter notes...
-		local rect_x1 = win_x + App.left_margin
-		local rect_y1 = win_y + App.top_margin - 5
+		local rect_x1 = App.arrange_win_x + App.left_margin
+		local rect_y1 = App.arrange_win_y + App.top_margin - 5
 		local rect_x2 = lane_end_x - 1
 		local rect_y2 = rect_y1 + 11 + (App.num_strings - 1) * App.lane_v_spacing
 		
 		if reaper.ImGui_IsWindowHovered(App.ctx) then
-			local m_x, m_y = reaper.ImGui_GetMousePos(App.ctx)
-			local cell_x = math.floor((m_x - win_x + scroll_x -15) / 34) - 1
-			local cell_y = math.floor((m_y - win_y - App.top_margin + 5) / 12)
+			local cell_x = math.floor((App.mouse_x - App.arrange_win_x + App.scroll_x -15) / 34) - 1
+			local cell_y = math.floor((App.mouse_y - App.arrange_win_y - App.top_margin + 5) / 12)
 			
-			if m_x > rect_x1 and m_x < rect_x2  and m_y > rect_y1 and m_y < rect_y2  then
-				local preview_x = win_x + App.left_margin + (cell_x * App.note_w) - scroll_x
-				local preview_y = win_y + App.top_margin + (cell_y * App.note_h) - 5
+			if App.mouse_x > rect_x1 and App.mouse_x < rect_x2  and App.mouse_y > rect_y1 and App.mouse_y < rect_y2  then
+				local preview_x = App.arrange_win_x + App.left_margin + (cell_x * App.note_w) - App.scroll_x
+				local preview_y = App.arrange_win_y + App.top_margin + (cell_y * App.note_h) - 5
 				reaper.ImGui_DrawList_AddRectFilled(draw_list, preview_x, preview_y, preview_x + App.note_w - 1, preview_y + App.note_h - 1, Colors.note_preview, 40)
 				
 				if reaper.ImGui_IsMouseClicked(App.ctx, 0) then
-					-- local temp = {offset = cell_x, string_idx = cell_y, num = 25, velocity = 127, duration = 1, selected = false}
-					-- App.note_list[#App.note_list+1] = temp
 					Editor.OnClick(cell_x, cell_y)
 				end
 			end
+		end
+		
+		-- These have prob have to go out of the drawing function
+		if reaper.ImGui_IsMouseReleased(App.ctx, 0) then
+			Editor.OnRelease()
+		end
+		
+		if reaper.ImGui_IsMouseDragging(App.ctx, 0) then
+			Editor.OnDrag()
 		end
 		
 		--------------------------------------------------------------------------------------------------------------
 		
 		
 		-- debug draw arrange mouse area
-		-- reaper.ImGui_DrawList_AddRect(draw_list, lane_start_x, win_y + App.top_margin - 5, lane_end_x + 1, win_y+App.top_margin - 5 + 11 + ((App.num_strings - 1) * App.lane_v_spacing), Colors.red)
+		-- reaper.ImGui_DrawList_AddRect(draw_list, lane_start_x, App.arrange_win_y + App.top_margin - 5, lane_end_x + 1, App.arrange_win_y+App.top_margin - 5 + 11 + ((App.num_strings - 1) * App.lane_v_spacing), Colors.red)
 		
 		-- Mask rect
-		reaper.ImGui_DrawList_AddRectFilled(draw_list, win_x, win_y+2, win_x+App.left_margin, win_y+140, Colors.bg)
+		reaper.ImGui_DrawList_AddRectFilled(draw_list, App.arrange_win_x, App.arrange_win_y+2, App.arrange_win_x+App.left_margin, App.arrange_win_y+140, Colors.bg)
 		
 		-- String legends
 		for i = 0, App.num_strings - 1 do
-			reaper.ImGui_DrawList_AddText(draw_list, win_x+8, win_y+23+ (i * App.lane_v_spacing), Colors.text, App.instrument[App.num_strings - 3][i + 1] .. " *")
+			local str = App.instrument[App.num_strings - 3][App.num_strings - i]
+			local len = string.len(str)
+			local space
+			if len == 2 then space = "  " else space = " " end
+			reaper.ImGui_DrawList_AddText(draw_list, App.arrange_win_x + 8, App.arrange_win_y + 23 + (i * App.lane_v_spacing), Colors.text, str .. space .. "*")
 		end
 		
 		reaper.ImGui_EndChild(App.ctx)
@@ -404,7 +497,8 @@ end
 function App.Loop()
 	App.is_visible, App.is_open = reaper.ImGui_Begin(App.ctx, App.window_title, true)
 	
-	if App.is_visible then		
+	if App.is_visible then
+		App.mouse_x, App.mouse_y = reaper.ImGui_GetMousePos(App.ctx)
 		App.window_w = reaper.ImGui_GetWindowWidth(App.ctx)
 		UI.DrawCB_Strings()
 		UI.DrawCB_Signature()
@@ -413,11 +507,12 @@ function App.Loop()
 		UI.DrawTXT_Help()
 		Util.HorSpacer(3)
 		if reaper.ImGui_Button(App.ctx, "Debug...") then
-			local temp = {offset = 8, string_idx = 3, num = 25, velocity = 127, duration = 1, selected = false}
-			App.note_list[#App.note_list+1] = temp
+			reaper.ShowConsoleMsg(Util.NoteNameToPitch("D3"))
 		end
 		UI.DrawArrange()
 		UI.DrawToolbar()
+		
+		App.mouse_prev_x, App.mouse_prev_y = App.mouse_x, App.mouse_y
 		reaper.ImGui_End(App.ctx)	
 	end
 	
